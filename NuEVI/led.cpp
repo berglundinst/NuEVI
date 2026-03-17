42d0537572435d4079c01050be0a61c562873f5f
John Stäck <john.stack@gmail.com>
2026-03-17 11:24:08 +0100

    More dead code removed

--- NuEVI/led.cpp
+++ NuEVI/led.cpp
@@ -49,13 +49,11 @@ void statusLedBlink() {
 
 void updateSensorLEDs() {
     if (breathLevel > breathThrVal) { // breath indicator LED, labeled "B" on PCB
-        //analogWrite(bLedPin, map(breathLevel,0,4096,5,breathLedBrightness));
         analogWrite(bLedPin, map(constrain(breathLevel, breathThrVal, breathMaxVal), breathThrVal, breathMaxVal, MIN_LED_BRIGHTNESS, BREATH_LED_BRIGHTNESS));
     } else {
         analogWrite(bLedPin, 0);
     }
     if (portIsOn) { // portamento indicator LED, labeled "P" on PCB
-        //analogWrite(pLedPin, map(biteSensor,0,4096,5,portamLedBrightness));
         analogWrite(pLedPin, map(constrain(oldport, 0, 127), 0, 127, MIN_LED_BRIGHTNESS, PORTAM_LED_BRIGHTNESS));
     } else {
         analogWrite(pLedPin, 0);
